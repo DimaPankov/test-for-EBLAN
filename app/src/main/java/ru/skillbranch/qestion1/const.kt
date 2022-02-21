dcd5391e47142f58e2025cc38f0b11bd1eee984f
DimaPankov <dimaray100500@gmail.com>
2022-02-22 03:27:44 +0700

    all work!

---
+++ app/src/main/java/ru/skillbranch/qestion1/const.kt
@@ -0,0 +1,8 @@
+package ru.skillbranch.qestion1
+
+var trueAnswerSize:Int = 0
+var falseAnswerSize:Int = 0
+
+
+
+lateinit var MAIN:MainActivity
---
+++ app/src/main/java/ru/skillbranch/qestion1/entities/entities.kt
@@ -0,0 +1,5 @@
+package ru.skillbranch.qestion1.entities
+
+import androidx.annotation.StringRes
+
+data class entities (@StringRes val textResId:Int, val imageResId:Int, val answer: Boolean)
